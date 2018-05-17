29de080083038bf6e45ed33360abaa3cf0f3286f
Trezy <tre@trezy.com>
2018-05-17 15:54:20 -0500

    Fix directory creation and abstract functions

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,21 +8,27 @@ function realpath() {
   echo $(cd "$(dirname "$1")" && pwd)/$(basename "$1");
 }
 
-function doIt() {
-  # Create required directories
-  for i in ('~/environments'); do
-    mkdir -p $i
-  done
 
-  # Create symlinks from ~ to each file in /symlinkable
-  for i in $(ls -A ./symlinkable); do
-    SYMLINK_PATH=$(realpath "./symlinkable/$i")
 
-    if [ ! -e ~/$i ]; then
-      ln -s $SYMLINK_PATH ~
+
+
+function addSourceablesToBashrc() {
+  # Source all the files in /sourceable from .bash_profile
+  for i in $(ls -pA ./sourceable | grep -v /); do
+    SOURCE_PATH=$(realpath "./sourceable/$i")
+    SOURCE_LINE="source $SOURCE_PATH"
+
+    if [ ! $(grep -q "$SOURCE_LINE" ~/.bash_profile) ]; then
+      echo "$SOURCE_LINE" >> ~/.bash_profile
     fi
   done
+}
 
+
+
+
+
+function createBashrc() {
   # Create a .bash_profile if we don't already have one
   if [ ! -e ~/.bash_profile ]; then
     touch ~/.bash_profile
@@ -38,33 +44,83 @@ function doIt() {
     echo "# Dotfiles" >> ~/.bash_profile
     echo "$SEPARATOR" >> ~/.bash_profile
   fi
+}
 
-  # Source all the files in /sourceable from .bash_profile
-  for i in $(ls -pA ./sourceable | grep -v /); do
-    SOURCE_PATH=$(realpath "./sourceable/$i")
-    SOURCE_LINE="source $SOURCE_PATH"
 
-    if [ ! $(grep -q "$SOURCE_LINE" ~/.bash_profile) ]; then
-      echo "$SOURCE_LINE" >> ~/.bash_profile
+
+
+
+function createFolders() {
+  # Create required directories
+  for i in '.environments'; do
+    mkdir -p "$HOME/$i"
+  done
+}
+
+
+
+
+
+function createSymlinks() {
+  # Create symlinks from ~ to each file in /symlinkable
+  for i in $(ls -A ./symlinkable); do
+    SYMLINK_PATH=$(realpath "./symlinkable/$i")
+
+    if [ ! -e ~/$i ]; then
+      ln -s $SYMLINK_PATH ~
     fi
   done
+}
 
+
+
+
+
+function handleSingleRunSourceables() {
   # Source all the files in /sourceable/once
-  # for i in $(ls -pA ./sourceable/once | grep -v /); do
-  #   SOURCE_PATH=$(realpath "./sourceable/once/$i")
+  for i in $(ls -pA ./sourceable/once | grep -v /); do
+    SOURCE_PATH=$(realpath "./sourceable/once/$i")
 
-  #   source $SOURCE_PATH
-  # done
+    source $SOURCE_PATH
+  done
+}
 
+
+
+
+
+function runSetupScripts() {
   # Run all of the setup scripts
   for i in $(ls -pA ./scripts | grep -v /); do
     SOURCE_PATH=$(realpath "./scripts/$i")
 
     $SOURCE_PATH
   done
+}
+
+
+
+
+
+function doIt() {
+  createFolders;
+
+  createSymlinks;
+
+  createBashrc;
+
+  addSourceablesToBashrc;
+
+  handleSingleRunSourceables;
+
+  runSetupScripts;
 
   source ~/.bash_profile;
 }
+
+
+
+
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
   doIt;
