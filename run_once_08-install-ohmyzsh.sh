6a7beb68441b00cffd0789f48b369dfe74fdd55b
Trezy <t@trezy.com>
2021-07-26 09:36:06 -0500

    add powerlevel10k installation

--- run_once_08-install-ohmyzsh.sh
+++ run_once_08-install-ohmyzsh.sh
@@ -5,7 +5,10 @@ ls ~/.oh-my-zsh &> /dev/null
 if [ $? -ne 0 ]; then
 	echo -n "Installing Oh My ZSH... "
 
-	sudo sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+	sudo sh -c "CHSH=yes RUNZSH=no KEEP_ZSHRC=yes $(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+	# Download the powerlevel10k theme
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 	echo "Done."
 fi
